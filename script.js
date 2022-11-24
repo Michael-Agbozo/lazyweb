c6fbb2aa291e0dddd24c746530373eb73fb19087
mykell_Writes <55024963+Michael-Agbozo@users.noreply.github.com>
2022-11-24 09:15:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var menulist = document.getElementById("menulist");
+
+menulist.style.maxHeight = "0px";
+
+function toggleMenu() {
+  if (menulist.style.maxHeight == "0px") {
+    menulist.style.maxHeight = "300px";
+  } else {
+    menulist.style.maxHeight = "0px";
+  }
+}
